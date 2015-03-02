76b9f561fb5730991ea16462ec873f72436fba76
sddioulde <sddioulde@hotmail.com>
2015-03-02 15:33:46 -0500

    First Commit

---
+++ src/UsefulMethods.java
@@ -0,0 +1,214 @@
+import java.util.List;
+import java.util.Stack;
+
+public class UsefulMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * List<Integer> l = new ArrayList<Integer>(); l.add(5); l.add(4);
+		 * l.add(34); l.add(1); l.add(33); printList(l); bubbleSort(l);
+		 * printList(l);
+		 * 
+		 * pascalsTriangle(5);
+		 * 
+		 * List<String> l = new ArrayList<String>(); l.add("Saliou");
+		 * l.add("Sup"); l.add("neyney");
+		 * 
+		 * binarySearch(l, "Sup");
+		 * 
+		 * expressionValidity("[()()][");
+		 * 
+		 * int[][] m = {{1,2},{0,1},{4,3}};
+		 * 
+		 * m = transposeMatrix(m); for(int[] a : m){ for(int b : a)
+		 * System.out.print(b + " "); System.out.println(); }
+		 * 
+		 * System.out.println(gcd(100,12));
+		 */
+	}
+
+	static int numCombinations(String s, int r) {
+		int num = 0;
+		int n = s.length();
+		// n! / r!(n-r)!
+
+		return num;
+	}
+
+	static int numPermutations(String s, int r) {
+		int num = 0;
+		int n = s.length();
+		// n! / (n-r)!
+
+		return num;
+	}
+
+	static int gcd(int a, int b) {
+		while (b != 0) {
+			int temp = b;
+			b = a % b;
+			a = temp;
+		}
+		return a;
+	}
+
+	static int[][] transposeMatrix(int[][] matrix) {
+		int numCols = matrix.length;
+		int numRows = matrix[0].length;
+		int[][] transpose = new int[numRows][numCols];
+		for (int row = 0; row < numCols; row++)
+			for (int col = 0; col < numRows; col++) {
+				transpose[col][row] = matrix[row][col];
+			}
+		matrix = transpose;
+
+		return matrix;
+	}
+
+	static void swap(int x, int y) {
+		int temp = x;
+		x = y;
+		y = temp;
+	}
+
+	static void expressionValidity(String s) {
+		Stack stack = new Stack();
+		boolean error = false;
+		for (char c : s.toCharArray()) {
+			if (c == '(' || c == '[')
+				stack.push(c);
+			else if (c == ')' || c == ']') {
+				if (stack.isEmpty()) {
+					System.out.println("The expression " + s
+							+ " is invalid. Not enough left parentheses.");
+					error = true;
+					break;
+				} else {
+					char pop = (char) stack.pop();
+					if (c == ')' && pop != '(' || c == ']' && pop != '[') {
+						System.out.println("The expression " + s
+								+ " is invalid. Mismatched parentheses.");
+						error = true;
+						break;
+					}
+				}
+			}
+		}
+		if (!error) {
+			if (stack.isEmpty())
+				System.out.println("The expression is valid!");
+			else
+				System.out.println("The expression " + s
+						+ " is invalid. Too many left parentheses.");
+		}
+	}
+
+	static void pascalsTriangle(int n) {
+		int[][] a = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				if (j == 0 || j == n)
+					a[i][j] = 1;
+				else
+					a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+			}
+		}
+		printList(a);
+	}
+
+	static void bubbleSort(List<Integer> l) {
+		boolean swapped = true;
+		for (int i = l.size() - 1; swapped && i >= 1; i--) {
+			swapped = false;
+			for (int j = 0; j < i; j++) {
+				if (l.get(j) > l.get(j + 1)) {
+					Integer temp = l.get(j);
+					l.set(j, l.get(j + 1));
+					l.set(j + 1, temp);
+					swapped = true;
+				}
+			}
+		}
+		System.out.println(l + "is sorted using bubble sort.");
+	}
+
+	static void binarySearch(List<Integer> l, int item) {
+		bubbleSort(l);
+		int a = 0;
+		int b = l.size();
+		while (a < b) {
+			int m = (a + b) / 2;
+			if (item <= l.get(m))
+				b = m;
+			else
+				a = m + 1;
+		}
+		if (item == l.get(a))
+			System.out.println(item + " is at index " + a + " in the list " + l
+					+ ".");
+		else
+			System.out.println(item + " is not in the list " + l + ".");
+	}
+
+	static void linearSearch(List<String> l, String item) {
+		boolean found = false;
+		for (String element : l) {
+			if (element.equals(item)) {
+				System.out.println("Found " + item + " at index "
+						+ l.indexOf(element) + ".");
+				found = true;
+			}
+		}
+		if (!found)
+			System.out.println("Did not find " + item + " in " + l + ".");
+	}
+
+	static void towersOfHanoi(int n, int x, int y, int z) {
+		if (n > 0) {
+			towersOfHanoi(n - 1, x, z, y);
+			System.out.println("Move ring " + n + " from " + x + " to " + y
+					+ ".");
+			towersOfHanoi(n - 1, z, y, x);
+		}
+	}
+
+	static int fibonacci(int n) {
+		int result = 0;
+
+		if (n == 0 || n == 1)
+			result = 1;
+		else
+			result = fibonacci(n - 1) + fibonacci(n - 2);
+
+		return result;
+	}
+
+	static int sumPowersOf2(int n) {
+		int x = 1;
+		int p = 1;
+
+		for (int i = 1; i <= n; i++) {
+			p *= 2;
+			x += p;
+		}
+
+		return x;
+	}
+
+	static void printList(List<Integer> l) {
+		for (Integer i : l)
+			System.out.print(i + " ");
+		System.out.println();
+	}
+
+	static void printList(int[][] a) {
+		for (int[] i : a) {
+			for (int j : i)
+				System.out.print(j + " ");
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
